8c0f9da24696fd3f03154d047c693e2a2f424f49
Rebecca Wilkerson <rueTH@users.noreply.github.com>
2017-05-01 08:36:55 -0500

    whoops, nm. not sure what i did with that save....

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,10 @@
-var CarLot = (function () {
-  var inventory = [];
-  return {
-    loadInventory: function (callback) {
-      var inventoryLoader = new XMLHttpRequest();
-      inventoryLoader.addEventListener("load", function () {
-      });
-    }
-  };
-})();
+function populatePage (inventory) {
+  // Loop over the inventory and populate the page
+  
+  // Now that the DOM is loaded, establish all the event listeners needed
+  CarLot.activateEvents();
+}
+
+// Load the inventory and send a callback function to be
+// invoked after the process is complete
+CarLot.loadInventory();
