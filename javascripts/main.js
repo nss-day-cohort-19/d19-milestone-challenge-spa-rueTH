655feaf6ff4fca1c873366293d3c03b2278ad16d
Rebecca Wilkerson <rueTH@users.noreply.github.com>
2017-05-06 21:31:17 -0500

    incomplete, having trouble viewing functions as subdivided as the README.md file asks it to be. currently writing it like I see it in my head, with the idea that, once the code is in front of me, I will be able to accomplish the appropriate subdivisions of functions by IIFE. referring back to a skipped exercise in order to accomplish this...because now it just feels disorganized. stepping away.

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@
 function populatePage (inventory) {
   // Loop over the inventory and populate the page  
     var container = document.getElementById("container");
-    var divVed = document.createElement("DIV");
+    var divVed = document.createElement("DIV").classList.add('carCards');
   for (var i = 0; i < inventory.length; i++) {
     var putTo_DivVed =
         `<div class="col-sm-3 offset-sm-1 bordered card" id="${inventory[i].year} ${inventory[i].model}">
@@ -18,6 +18,45 @@ function populatePage (inventory) {
   } 
   // Now that the DOM is loaded, establish all the event listeners needed
   //<element>.addEventListenter('<event>', function)
+  
+  divVed[i].addEventListener('hover', cardHover);
+  divVed[i].addEventListener('click', cardClick);
+    var activateEvents = function() {
+      var carCards = document.getElementsByClassName("carCards");
+      // var textInput = document.getElementById("????");
+      for (var i = 0; i < carCards.length; i++) {
+        carCards[i].addEventListener('click', (event) => {
+          console.log(event.curentTarget.classList.contains("cardClick")) {
+            if (!event.currentTarget.classList.contains("cardClick")) {
+              for (var k = 0; k < carCards.length; k++) {
+                carCards[k].classList.remove("cardClick");
+              }
+              CarLot.clickBorder(event.currentTarget, "cardClick");
+            } else {
+              CarLot.borderRevert(event.currentTarget, "cardClick");
+            }
+          }
+        })
+        carCards[i].addEventListener('hover', (event) => {
+          console.log(event.currentTarget.classList.contains("cardHover")) {
+            if (!event.currentTarget.classList.contains("cardHover")) {
+              //k is for previously targeted card with augmented boarder (via event.classList.add())
+              for (var k = 0; k < carCards.length; k++) {
+                carCards[k].classList.remove("cardHover");
+              }
+              CarLot.hoverBoarder(event.currentTarget, "cardHover");
+            } else {
+              CarLot.borderRevert(event.currentTarget, "cardHover");
+            }
+          }
+        })
+      }
+    }
+    function cardHover() {
+      // ____.classList.add('cardHover')
+      // for (i = )
+    }
+
   // CarLot.activateEvents();
 }
 
